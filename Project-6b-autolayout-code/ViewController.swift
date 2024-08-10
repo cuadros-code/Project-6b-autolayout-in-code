ce560b686500ee4188d882e4e17019125c98fce5
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-10 11:36:16 -0500

    Initial Commit

---
+++ Project-6b-autolayout-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-6b-autolayout-code
+//
+//  Created by Kevin Cuadros on 10/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
